71879af52784d39243f645b9e6f2b27cb3b44182
dgladyshev <dmitry.gladishev@gmail.com>
2017-03-21 19:38:14 +0300

    REST improvements

--- src/main/java/com/dgladyshev/deadcodedetector/controllers/DeadCodeController.java
+++ src/main/java/com/dgladyshev/deadcodedetector/controllers/DeadCodeController.java
@@ -1,8 +1,7 @@
 package com.dgladyshev.deadcodedetector.controllers;
 
+import com.dgladyshev.deadcodedetector.entity.Check;
 import com.dgladyshev.deadcodedetector.entity.CheckRequest;
-import com.dgladyshev.deadcodedetector.entity.CheckStatus;
-import com.dgladyshev.deadcodedetector.entity.RepositoryCheckStatus;
 import com.dgladyshev.deadcodedetector.services.CheckCodeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,30 +9,45 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 @Slf4j
 @RestController
+@RequestMapping("/api/v1/")
 public class DeadCodeController {
 
 	@Autowired
 	CheckCodeService checkCodeService;
 
-	@RequestMapping(value = {"/health"}, method = RequestMethod.GET)
-	public String healthCheck() {
-		//TODO implement smart health check
-		return "OK";
-	}
-
-	@RequestMapping(value = "api/v1/repositories", method = RequestMethod.POST)
+	@RequestMapping(value = "/checks", method = RequestMethod.POST)
 	public
 	@ResponseBody
-	ResponseEntity<RepositoryCheckStatus> addCheckRequest(@RequestBody CheckRequest checkRequest) {
+	ResponseEntity<Check> addCheck(@RequestBody CheckRequest checkRequest) {
 		log.info("Incoming request: " + checkRequest);
 		//TODO check that request is valid
-		//TODO generate id for repository check
-		String checkId = java.util.UUID.randomUUID().toString(); //TODO generate unique id
-		//TODO process request async
-		checkCodeService.checkCode(checkId, checkRequest.getUrl(), checkRequest.getLanguage());
-		return new ResponseEntity<>(new RepositoryCheckStatus(checkId, CheckStatus.ADDED), HttpStatus.OK);
+		Check check = checkCodeService.createCheck(checkRequest.getUrl(), checkRequest.getLanguage());
+		checkCodeService.checkCode(check.getCheckId());
+		return new ResponseEntity<>(check, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/checks", method = RequestMethod.GET)
+	public
+	@ResponseBody
+	ResponseEntity<Collection<Check>> getChecks() {
+		return new ResponseEntity<>(
+				checkCodeService.getChecks().values(),
+				HttpStatus.OK
+		);
+	}
+
+	@RequestMapping(value = "/checks/{id}", method = RequestMethod.GET)
+	public
+	@ResponseBody
+	ResponseEntity<Check> getCheckById(@PathVariable String id) {
+		return new ResponseEntity<>(
+				checkCodeService.getCheckById(id),
+				HttpStatus.OK
+		);
 	}
 
 }
--- src/main/java/com/dgladyshev/deadcodedetector/services/CheckCodeService.java
+++ src/main/java/com/dgladyshev/deadcodedetector/services/CheckCodeService.java
@@ -1,14 +1,50 @@
 package com.dgladyshev.deadcodedetector.services;
 
+import com.dgladyshev.deadcodedetector.entity.Check;
+import com.dgladyshev.deadcodedetector.entity.CheckStatus;
+import com.dgladyshev.deadcodedetector.exceptions.NoSuchCheckException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class CheckCodeService {
 
+	private ConcurrentHashMap<String, Check> checks = new ConcurrentHashMap<>();
+
 	@Async
-	public void checkCode(String checkId, String url, String language) {
+	public void checkCode(String checkId) {
+		Check check = checks.get(checkId);
 		//TODO implement
+	}
+
+	public Check createCheck(String url, String language) {
+		String checkId = java.util.UUID.randomUUID().toString(); //TODO generate unique id
+		checks.put(
+				checkId,
+				Check.builder()
+						.checkId(checkId)
+						.repoUrl(url)
+						.repoLanguage(language)
+						.timeAdded(System.currentTimeMillis())
+						.checkStatus(CheckStatus.ADDED)
+						.build()
+		);
+		return checks.get(checkId);
+	}
+
+	public Map<String, Check> getChecks() {
+		return checks;
+	}
+
+	public Check getCheckById(String id) throws NoSuchCheckException {
+		if (checks.containsKey(id)) {
+			return checks.get(id);
+		} else {
+			throw new NoSuchCheckException();
+		}
 	}
 
 }
