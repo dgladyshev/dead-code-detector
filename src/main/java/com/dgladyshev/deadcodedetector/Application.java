77c2aa62cb1ac579d77234f5f2adb933d96684ee
dgladyshev <dmitry.gladishev@gmail.com>
2017-03-23 17:53:11 +0300

    add improve controller tests

--- src/main/java/com/dgladyshev/deadcodedetector/Application.java
+++ src/main/java/com/dgladyshev/deadcodedetector/Application.java
@@ -7,14 +7,9 @@ import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
@@ -30,15 +25,6 @@ public class Application {
 	@Bean
 	public ConversionService conversionService() {
 		return new DefaultConversionService();
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-		mappingJackson2HttpMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM));
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
-		return restTemplate;
 	}
 
 }
--- src/test/java/com/dgladyshev/deadcodedetector/controllers/DeadCodeControllerTest.java
+++ src/test/java/com/dgladyshev/deadcodedetector/controllers/DeadCodeControllerTest.java
@@ -1,55 +1,83 @@
 package com.dgladyshev.deadcodedetector.controllers;
 
+import com.dgladyshev.deadcodedetector.entity.Inspection;
 import com.dgladyshev.deadcodedetector.services.InspectionService;
+import com.dgladyshev.deadcodedetector.util.URLChecker;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @ActiveProfiles(profiles = {"local"})
-@RunWith(SpringRunner.class)
+@RunWith(PowerMockRunner.class)
 @WebMvcTest(DeadCodeController.class)
+@WebAppConfiguration
+@PrepareForTest({URLChecker.class})
 public class DeadCodeControllerTest {
 
-	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
+	@InjectMocks
+	private DeadCodeController deadCodeController;
+
+	@Mock
 	private InspectionService inspectionService;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = standaloneSetup(deadCodeController).build();
+		mockStatic(URLChecker.class);
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	public void testGetInspections() throws Exception {
 		this.mockMvc.perform(get("/api/v1/inspections")
-				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk());
 	}
 
 	@Test
 	public void testGetInspectionById() throws Exception {
 		this.mockMvc.perform(get("/api/v1/inspections/someId")
-				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk());
 	}
 
 	@Test
 	public void testDeleteInspectionById() throws Exception {
-		this.mockMvc.perform(get("/api/v1/inspections/someId")
-				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+		this.mockMvc.perform(delete("/api/v1/inspections/someId")
+				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk());
 	}
 
 	@Test
 	public void testAddInspectionById() throws Exception {
-		this.mockMvc.perform(get("/api/v1/inspections?url=https://github.com/dgladyshev/dead-code-detector.git&language=JAVA")
-				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+		//TODO improve
+		when(inspectionService.createInspection(any())).thenReturn(
+				Inspection.builder().inspectionId("someId").build()
+		);
+		this.mockMvc.perform(post("/api/v1/inspections")
+				.param("url", "https://github.com/dgladyshev/dead-code-detector.git")
+				.param("language", "JAVA")
+				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(status().isOk());
 	}
 
