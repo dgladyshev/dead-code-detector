82eb449d7618a9cb42b362bd067755f0c3b0ede5
dgladyshev <dmitry.gladishev@gmail.com>
2017-03-24 20:28:25 +0300

    beutify swagger page

--- src/main/java/com/dgladyshev/deadcodedetector/Application.java
+++ src/main/java/com/dgladyshev/deadcodedetector/Application.java
@@ -1,5 +1,6 @@
 package com.dgladyshev.deadcodedetector;
 
+import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,12 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -24,6 +31,30 @@ public class Application {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Dead Code Detector")
+                .description("Application that detects dead code in any public git repository.")
+                .contact(
+                        new Contact(
+                                "Dmitry Gladyshev",
+                                null,
+                                "dmitry.gladishev@aurea.com"
+                        )
+                )
+                .version("1.0")
+                .build();
     }
 
 }
